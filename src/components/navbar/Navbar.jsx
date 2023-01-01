76496df383b9cd5381e002b1ce1fc7ba37df1a76
Aleksandr Ural <aleksandrural@hotmail.com>
2023-01-01 16:06:35 +0000

    feat: removed navbar menu animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
         )}
 
         {toggleMenu && (
-          <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container">
             <div className="gpt3__navbar-menu_container-links">
               <Menu />
               <div className="gpt3__navbar-menu_container-links-sign">
